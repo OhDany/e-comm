b946225d12423138255017946c70854bf6d3da05
Daniel Ortega <ohdany@iMac.local>
2020-04-29 12:05:11 -0500

    Creating a Web Server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hola');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('hi Oh Dany');
+});
+
+app.listen(3000, () => {
+	console.log('Listening...');
+});
