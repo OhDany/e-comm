7672e564a37c448df15dbd7acd725754a2f0688b
Daniel Ortega <ohdany@iMac.local>
2020-04-30 18:01:14 -0500

    Opening the Repo Data File

--- repositories/users.js
+++ repositories/users.js
@@ -13,6 +13,22 @@ class UserRepository {
 			fs.writeFileSync(this.filename, '[]');
 		}
 	}
+
+	async getAll() {
+		return JSON.parse(
+			await fs.promises.readFile(this.filename, {
+				encoding: 'utf8'
+			})
+		);
+	}
 }
 
-const repo = new UserRepository('user.json');
+const test = async () => {
+	const repo = new UserRepository('user.json');
+
+	const users = await repo.getAll();
+
+	console.log(users);
+};
+
+test();
