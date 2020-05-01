15e6384c7a22530cbecaf338a3a3d029e5982c18
Daniel Ortega <oh.dany.mx@gmail.com>
2020-04-30 19:47:39 -0500

    Updating Recirds

--- repositories/users.js
+++ repositories/users.js
@@ -50,12 +50,24 @@ class UserRepository {
 		const filteredRecords = records.filter((record) => record.id !== id);
 		await this.writeAll(filteredRecords);
 	}
+
+	async update(id, attrs) {
+		const records = await this.getAll();
+		const record = records.find((record) => record.id === id);
+
+		if (!record) {
+			throw new Error(`Record with id ${id} not found`);
+		}
+
+		Object.assign(record, attrs);
+		await this.writeAll(records);
+	}
 }
 
 const test = async () => {
 	const repo = new UserRepository('user.json');
 
-	await repo.delete('007d64ee');
+	await repo.update('57701f40', { password: 'muynewpasswordOhDany' });
 };
 
 test();
