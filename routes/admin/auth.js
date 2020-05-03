f6d220566a4116158e7848b0819f23100f59e3c5
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-03 11:34:24 -0500

    Receiving Validation Output

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const siginTempalte = require('../../views/admin/auth/signin');
@@ -9,26 +11,37 @@ router.get('/signup', (req, res) => {
 	res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-	const { email, password, passwordConfirmation } = req.body;
+router.post(
+	'/signup',
+	[
+		check('email').trim().normalizeEmail().isEmail(),
+		check('password').trim().isLength({ min: 4, max: 20 }),
+		check('passwordValidation').trim().isLength({ min: 4, max: 20 })
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		console.log(errors);
 
-	const existingUser = await usersRepo.getOneBy({ email });
-	if (existingUser) {
-		return res.send('Email in use');
+		const { email, password, passwordConfirmation } = req.body;
+
+		const existingUser = await usersRepo.getOneBy({ email });
+		if (existingUser) {
+			return res.send('Email in use');
+		}
+
+		if (password !== passwordConfirmation) {
+			return res.send('Password mus match');
+		}
+
+		// Create a user in our user repo to represent this person
+		const user = await usersRepo.create({ email, password });
+
+		// Store the id of tahat inside the users cookie
+		req.session.userId = user.id;
+
+		res.send('Accoun created!!!');
 	}
-
-	if (password !== passwordConfirmation) {
-		return res.send('Password mus match');
-	}
-
-	// Create a user in our user repo to represent this person
-	const user = await usersRepo.create({ email, password });
-
-	// Store the id of tahat inside the users cookie
-	req.session.userId = user.id;
-
-	res.send('Accoun created!!!');
-});
+);
 
 router.get('/signout', (req, res) => {
 	req.session = null;
